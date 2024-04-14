2a355a9df915d62c99ffcfd73ca10462252b1450
Jonnathan1989 <jonnathanreys@gmail.com>
2024-04-13 20:26:05 -0500

    Esta es mi app con Reac Native

--- App.js
+++ App.js
@@ -1,20 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity, Alert, TextInput } from 'react-native'
+import image from './assets/logosena.png'
+import { Button } from "react-native-web";
 
-export default function App() {
+const Separator = () => <View style={styles.separator} />;
+
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={image}
+        style={styles.Image}
+      />
+      <Text style={styles.title}>Bienvenido</Text>
+      <Text style={styles.subTitle}>Aprendíz ficha: 2627092 </Text>
+      <Text style={styles.subTitle}> Inicia sesión</Text>
+      <TextInput
+        placeholder='jonnathanreys@gmail.com'
+        style={styles.TextInput}
+      />
+      <TextInput
+        placeholder='password'
+        style={styles.TextInput}
+      />
+      <TouchableOpacity
+        onPress={() => Alert.alert('Jonnathan Reyes Mosquera')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => Alert.alert('Ficha: 2627092')}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Salir</Text>
+      </TouchableOpacity>
+      <Separator />
+      <Text style={styles.piePagina}> No tienes cuenta? se te olvido la contraseña?</Text>
     </View>
   );
-}
+
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: "#f0fff0" },
+  title: { 
+    fontSize: 35,
+    color: 'gray' 
   },
-});
+  subTitle:{
+    fontSize: 20,
+    color:'gray',
+    justifyContent: 'center'
+  },
+  piePagina:{
+    fontSize: 15,
+    color:'gray',
+    justifyContent: 'center'
+  },
+  Image: { height: 200, width: 200, borderRadius: 100 },
+  button: {
+    backgroundColor: '#008000',
+    padding: 10,
+    marginTop: 25,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#ffff',
+    marginHorizontal: 30,
+    fontSize: 25,
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: '#000',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+
+  TextInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    paddingStart: 50,
+    width: '90%',
+    height: 50,
+    marginTop: 25,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  },
+
+})
+
+export default App;
